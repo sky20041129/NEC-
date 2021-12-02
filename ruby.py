cd3b9ffdb47b0f836a9c44bef29cc5d5adf8d2b4
sky20041129 <46390548+sky20041129@users.noreply.github.com>
2021-12-02 17:03:44 +0800

    Update ruby.py

--- ruby.py
+++ ruby.py
@@ -2,19 +2,20 @@ import os
 import time
 from datetime import datetime
 
-
-while True:
-    print ("wait progress 10 m")
-    localtime = time.localtime()
-    result = time.strftime("%Y-%m-%d %I:%M:%S %p", localtime)
-    print(result)
-    time.sleep(30)
-    with open('C:/Program Files (x86)/U R U/server/raid.log') as f:
-        if 'Initiaze start' in f.read():
-            print("pass")
-            break
-
-        else:
-            continue
-
-    os.system("storcli /c0/v4 start cc")
+def BGT_disktest():
+    while True:
+        print ("wait progress 10 m")
+        localtime = time.localtime()
+        result = time.strftime("%Y-%m-%d %I:%M:%S %p", localtime)
+        print(result)
+        time.sleep(3)
+        with open('C:/Program Files (x86)/Universal RAID Utility/server/raid.log') as f:
+            if 'Initiaze start' in f.read():
+                print("pass")
+                break
+    
+            else:
+                continue
+    
+        #os.system("storcli /c0/v4 start cc")
+BGT_disktest()
